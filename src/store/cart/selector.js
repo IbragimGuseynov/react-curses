ea29de555049ad7b633835e903e6a9942c8f5e88
CORP\ibragim.guseynov <ibragim.guseynov@ax-team.com>
2023-03-15 14:12:42 +0300

    delete extra

--- src/store/cart/selector.js
+++ src/store/cart/selector.js
@@ -3,8 +3,5 @@ export const selectCartModule = (state) => state.cart;
 export const selectDishCount = (state, { dishName }) =>
   selectCartModule(state)[dishName] || 0;
 
-export const selectDishById = (state, { dishId }) =>
-  selectCartModule(state)[dishId] || 0;
-
 export const selectCartEntries = (state) =>
   Object.entries(selectCartModule(state));
